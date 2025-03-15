7bfd4071642effa15787a6a3dd63347e24f4e569
GrumpyBud <2028thao@webbstudent.org>
2025-03-15 18:37:47 -0400

    Revert "it works gooder now"
    This reverts commit 9f245be35bbf2316f5438382e3f5c232e8087a52.

--- Webb-Swerve-Lib/src/main/java/org/webbrobotics/frc2025/BuildConstants.java
+++ Webb-Swerve-Lib/src/main/java/org/webbrobotics/frc2025/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Webb-Swerve-Lib";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "1bb9a53df77dac8b21284e72e861e648c2832123";
-  public static final String GIT_DATE = "2025-03-15 11:45:18 EDT";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "6585b7390e6dfff331e5595158fec7bcdfb95744";
+  public static final String GIT_DATE = "2025-03-12 13:47:27 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-15 15:10:56 EDT";
-  public static final long BUILD_UNIX_TIME = 1742065856364L;
+  public static final String BUILD_DATE = "2025-03-15 11:28:49 EDT";
+  public static final long BUILD_UNIX_TIME = 1742052529122L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
